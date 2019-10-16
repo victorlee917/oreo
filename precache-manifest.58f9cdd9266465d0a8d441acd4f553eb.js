8b7a6019cf6cc270c3b651338ea16638177e2dc0
victorlee917 <41850048+victorlee917@users.noreply.github.com>
2019-10-17 00:39:38 +0900

    Updates

---
+++ precache-manifest.58f9cdd9266465d0a8d441acd4f553eb.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb04837710eaa778f270a188e73f1a2e",
+    "url": "/oreo/index.html"
+  },
+  {
+    "revision": "0886610039b66a06c19c",
+    "url": "/oreo/static/js/2.a2acff43.chunk.js"
+  },
+  {
+    "revision": "50d974c074146d267b2f",
+    "url": "/oreo/static/js/main.8a9765cb.chunk.js"
+  },
+  {
+    "revision": "4de803d2f93eed3253b9",
+    "url": "/oreo/static/js/runtime-main.725f30ce.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.09596aab1df0706c7f4008ce50652c77.js"
+  "/oreo/precache-manifest.58f9cdd9266465d0a8d441acd4f553eb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/oreo/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
