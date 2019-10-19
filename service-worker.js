e4fa3cfbe89e9b1eb3330a39bb9499ae2f7006b7
victorlee917 <41850048+victorlee917@users.noreply.github.com>
2019-10-19 13:40:40 +0900

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/oreo/precache-manifest.5d5112faeaee117c55a81b3b74ed8807.js"
+  "/oreo/precache-manifest.c879b831ae418403670c0aa8da6909ae.js"
 );
 
 self.addEventListener('message', (event) => {
